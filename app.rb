fed8f3629624111688e89068636aa993ec5014a9
Zoe Cohen <cohen.zoe@berkeley.edu>
2023-07-03 01:01:03 +0000

    Added rerun gem to development block of gemfile

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
   get '/' do
-    "<!DOCTYPE html><html><head></head><body><h1>Hello World</h1></body></html>"
+    "<!DOCTYPE html><html><head></head><body><h1>Gday Mate</h1></body></html>"
   end
 end
